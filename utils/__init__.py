0627129f6e77b6a190e482c9af5a218b76fd656d
Arnaud Petit <arnaud.petit39@gmail.com>
2022-06-15 14:42:04 -0400

    Update __init__.py
    add the import of new pipe,e

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .signal_filtering.functions import filter_joint, filter_one, filter
 from .signal_filtering.filtering_methods import hpfilter, kernel, seasonalfilter
 from .pipeline.cdc_wonder import auto_wonder_pipeline
+from .pipeline.cdc_wonder_2018 import auto_wonder_2018_pipeline
 from .regression_discontinuity.data_processing import process_data_rd
